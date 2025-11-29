d19119b52c426587df66f815d9bd961092264992
Lucca Defelippo de Paula Romanelli <48338875+LuccaRomanelli@users.noreply.github.com>
2025-11-29 16:01:51 -0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install all packages in order
+./install-zsh.sh
+./install-yazi.sh
+./install-asdf.sh
+./install-nodejs.sh
+./install-php.sh
+./install-ghostty.sh
+./install-tmux.sh
+./install-stow.sh
+./install-dotfiles.sh
+./install-shell-scripts.sh
+./install-ripgrep.sh
+./install-obsidian-vault.sh
+./install-hyprland-overrides.sh
+./install-ai-tools.sh
+./install-omarchy-chromium-google-account.sh
+./install-omarchy-themes.sh
+./install-omarchy-webapps.sh
+./install-todoist.sh
+./install-stow.sh
+./install-dotfiles.sh
+./set-shell.sh
+./unistall-omarchy-apps.sh
+./unistall-omarchy-webapps.sh

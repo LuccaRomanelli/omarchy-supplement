4b24aedc26f0303f0f5f21bb82c80641bc03ed65
Lucca Romanelli <ldpromanelli@gmail.com>
2025-11-30 12:38:22 -0300

    fix dev

---
+++ install-dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! command -v node &>/dev/null; then
+    omarchy-install-dev-env node
+fi
+
+if ! command -v laravel &>/dev/null; then
+    omarchy-install-dev-env laravel
+fi
+
+if ! command -v pnpm &>/dev/null; then
+    yay -S --noconfirm --needed pnpm
+fi

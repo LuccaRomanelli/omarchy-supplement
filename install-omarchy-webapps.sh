6db4dec339a385ee7dc88f4d1b3a2b87f008e635
Lucca <ldpromanelli@gmail.com>
2025-11-30 00:17:33 -0300

    supp

--- install-omarchy-webapps.sh
+++ install-omarchy-webapps.sh
@@ -4,8 +4,7 @@ omarchy-webapp-install "ChatLLM" https://apps.abacus.ai/chatllm/ "https://cdn.js
 omarchy-webapp-install "Calendar" https://calendar.google.com/calendar "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-calendar.png"
 omarchy-webapp-install "Music" https://music.youtube.com/ "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube-music.png"
 omarchy-webapp-install "Clickup" https://app.clickup.com/ "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/clickup.png"
-omarchy-webapp-install "Google Chat" https://app.clickup.com/ "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/clickup.png"
-omarchy-webapp-install "Google Chat" https://app.clickup.com/ "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/clickup.png"
+omarchy-webapp-install "Google Chat" https://mail.google.com/ "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-chat.png"
 omarchy-webapp-install "GitLab" https://gitlab.com/ "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/gitlab.png"
 
 

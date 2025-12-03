a769ec0a025f446149a35841c8a3ffdf607ac4f1
Lucca Defelippo de Paula Romanelli <48338875+LuccaRomanelli@users.noreply.github.com>
2025-12-03 15:48:05 -0300

    Update install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -3,4 +3,5 @@
 # Install Zsh
 if ! command -v zsh &>/dev/null; then
     yay -S --noconfirm --needed zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi

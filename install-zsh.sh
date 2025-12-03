24808e32ef9d87066390a4fae5db13e3e8006ecf
Lucca Romanelli <ldpromanelli@gmail.com>
2025-12-03 17:17:40 -0300

    fix

--- install-zsh.sh
+++ install-zsh.sh
@@ -4,4 +4,8 @@
 if ! command -v zsh &>/dev/null; then
     yay -S --noconfirm --needed zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
